cc5b51192c4f534bc345fa1f4711cb500f8e5309
liuhuan <liuhuan@133.cn>
2017-05-09 08:52:32 +0800

    simple

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,7 +3,6 @@ import classNames from 'classnames'
 import localize from '../../utils/hoc/localize'
 import UsersStore from 'lib/users/store'
 import UsersActions from 'lib/users/actions'
-import Count from 'components/count'
 import fetch from 'node-fetch'
 
 class Home extends Component {
@@ -14,16 +13,6 @@ class Home extends Component {
       total: 0,
       name: 'levuon'
     }
-    this.show = this.show.bind( this )
-    this._updateSiteUsers = this._updateSiteUsers.bind( this )
-  }
-
-  async componentWillMount () {
-    // UsersStore.on( 'change', this._updateSiteUsers );
-    // UsersActions.fetchUsers();
-    const  url = `https://api.github.com/users/${this.state.name}`;
-    const response = await fetch( url );
-    console.log(response)
   }
 
   _getState () {
@@ -36,19 +25,8 @@ class Home extends Component {
     } );
   }
 
-  _updateSiteUsers () {
-    this.setState( this._getState() )
-  }
-
-  show () {
-    const { show } = this.props;
-    show();
-  }
-
   render () {
-
     const classes = classNames( this.props.className, {
-      'is-loading': !this.props.post || !this.props.connections
     } );
     return (
      <div onClick = {this.show} >
@@ -58,7 +36,6 @@ class Home extends Component {
          )
        } )}</h1>
        <h1>{this.state.total}</h1>
-       <Count count = {2222} />
      </div>
     )
   }
